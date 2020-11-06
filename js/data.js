3fd80a310a3d9ba8cb04ae6292f5a61388acf742
Niseme <71128250+Niseme@users.noreply.github.com>
2020-11-06 14:47:35 +0200

    paruosti duomenys

---
+++ js/data.js
@@ -0,0 +1,30 @@
+const kirpyklaData = [
+    {
+        label: 'Kirpimo pavadinimas',
+        description: 'Aprasymas',
+        price: 0.00,
+        link: '#'
+    },
+    {
+        label: 'Kirpimo pavadinimas1',
+        description: 'Aprasymas1',
+        price: 1.00,
+        link: '#'
+    },
+    {
+        label: 'Kirpimo pavadinimas2',
+        description: 'Aprasymas2',
+        price: 2.00,
+        link: '#'
+    },
+    {
+        label: 'Kirpimo pavadinimas3',
+        description: 'Aprasymas3',
+        price: 3.00,
+        link: '#'
+    },
+];
+
+export { kirpyklaData }
+
+
